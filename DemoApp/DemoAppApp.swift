1e73560c4e9da98064793b920e5f77635db57947
Ibrahim Mohammed Gedami <hemagedamy@gmail.com>
2025-08-25 13:25:40 +0400

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Ibrahim Mo Gedami on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoAppTests/DemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppTests.swift
+//  DemoAppTests
+//
+//  Created by Ibrahim Mo Gedami on 25/08/2025.
+//
+
+import Testing
+@testable import DemoApp
+
+struct DemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
